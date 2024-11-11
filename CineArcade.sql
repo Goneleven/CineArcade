1426325b32fb2d9f2515a3adddb820b9dd049b1a
Natan Fernando <natanfernandos26@gmail.com>
2024-11-11 16:30:56 -0300

    Add files via upload

--- CineArcade.sql
+++ CineArcade.sql
@@ -9,48 +9,76 @@ USE CINEARCADE;
 -- CRIAÇÃO DA TABELA 'CLIENTES'
 
 CREATE TABLE Clientes(
-    cpf             VARCHAR(14) PRIMARY KEY,
+    cpf             VARCHAR(14) PRIMARY KEY NOT NULL,
     nome            VARCHAR(60) NOT NULL,
     telefone        VARCHAR(15) NOT NULL,
     email           VARCHAR(40) NOT NULL,
-    endereco        VARCHAR(70) NOT NULL,
-    cep             VARCHAR(9) NOT NULL,
-    cidade          VARCHAR(30) NOT NULL,
-    uf              VARCHAR(2) NOT NULL,
-    complemento     VARCHAR(50),
-    data_nascimento DATE
+    data_nascimento DATE,
+    id_endereco 	INT NOT NULL,
+    CONSTRAINT fk_id_endereco FOREIGN KEY (id_endereco) REFERENCES Enderecos(id_endereco)
 );
 
 -- DADOS DOS CLIENTES
 
-INSERT INTO Clientes(cpf, nome, telefone, email, endereco, cep, cidade, uf, complemento, data_nascimento) 
+INSERT INTO Clientes(cpf, nome, telefone, email, data_nascimento, id_endereco) 
 VALUES 
-	('104.092.918-46', 'José Santana Pereira', '11 2350-7973', 'josesantana004@outlook.com', 'Rua Professor Jurandyr de Oliveira, Jardim Esperança, 888', '08743-180', 'Mogi Das Cruzes', 'SP', 'Casa Portão Vermelho', '1974-03-17'),
-	('099.784.428-04', 'Elza Sophia Lúcia Corte Real', '11 2512-1938', 'Elzasophiaa123@gmail.com', 'Avenida Capitão Arcílio Rizzi, Cézar de Souza, 798', '08820-130', 'Mogi Das Cruzes', 'SP', 'Condominio A', '1956-02-20'),
-	('255.527.438-30', 'Manoel Ryan Yago Rocha', '11 3543-8447', 'Manoelryanrocha@outlook.com', 'Rua Loyde Aéreo, Jardim Aeroporto III, 579', '08761-500', 'Mogi Das Cruzes', 'SP', 'Condominio Bloco 7', '1985-07-13'),
-    ('826.387.678-83', 'Emanuel Rafael da Rocha', '11 2670-0066', 'Emanuelrocha444@gmail.com', 'Rua João Ribeiro de Brito, Vila Paulista, 546', '08744-030', 'Mogi Das Cruzes', 'SP', 'Villagio Vivendas', '1988-08-19'),
-	('715.395.898-38', 'Lara Caroline Souza', '11 2791-2532', 'Laracarolsouzaa@gmail.com', 'Rua Davi Duran, Mogi Moderno, 783', '08717-445', 'Mogi Das Cruzes', 'SP', 'Casa', '1999-01-11');
+	('104.092.918-46', 'José Santana Pereira', '11 2350-7973', 'josesantana004@outlook.com', '1974-03-17', 1),
+	('099.784.428-04', 'Elza Sophia Lúcia Corte Real', '11 2512-1938', 'Elzasophiaa123@gmail.com', '1956-02-20', 2),
+	('255.527.438-30', 'Manoel Ryan Yago Rocha', '11 3543-8447', 'Manoelryanrocha@outlook.com', '1985-07-13', 3),
+    ('826.387.678-83', 'Emanuel Rafael da Rocha', '11 2670-0066', 'Emanuelrocha444@gmail.com', '1988-08-19', 4),
+	('715.395.898-38', 'Lara Caroline Souza', '11 2791-2532', 'Laracarolsouzaa@gmail.com', '1999-01-11', 5);
 
 SELECT * FROM Clientes;
 
 DESCRIBE Clientes;
 
--- CRIAÇÃO DA TABELA 'CARGO' 
- 
-CREATE TABLE Cargo(
-    id_cargo INT PRIMARY KEY,
-    funcoes  VARCHAR(30)
+-- CRIAÇÃO DA TABELA 'ENDEREÇOS'
+
+CREATE TABLE Enderecos(
+	id_endereco INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    numero INT(10) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+    complemento VARCHAR(100) NOT NULL
 );
 
--- DADOS DE CARGO
- 
-INSERT INTO Cargo(id_cargo, funcoes)
+-- DADOS DE ENDEREÇOS
+
+INSERT INTO Enderecos(logradouro, numero, cep, cidade, uf, complemento) 
 VALUES 
-	(01, 'Diretor'),
-	(02, 'Gerente'),
-	(03, 'Supervisor'),
-	(04, 'Assistente Geral'),
-	(05, 'Atendente');
+	('Rua Professor Jurandyr de Oliveira, Jardim Esperança', 888, '08743-180', 'Mogi Das Cruzes', 'SP', 'Casa Portão Vermelho'),
+	('Avenida Capitão Arcílio Rizzi, Cézar de Souza', 798, '08820-130', 'Mogi Das Cruzes', 'SP', 'Condominio A'),
+	('Rua Loyde Aéreo, Jardim Aeroporto III', 579, '08761-500', 'Mogi Das Cruzes', 'SP', 'Condominio Bloco 7'),
+    ('Rua João Ribeiro de Brito, Vila Paulista', 546, '08744-030', 'Mogi Das Cruzes', 'SP', 'Villagio Vivendas'),
+	('Rua Davi Duran, Mogi Moderno', 783, '08717-445', 'Mogi Das Cruzes', 'SP', 'Casa');
+
+SELECT * FROM Enderecos;
+
+DESCRIBE Enderecos;
+
+-- CRIAÇÃO DA TABELA 'CARGOS' 
+ 
+CREATE TABLE Cargos(
+    id_cargo INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    funcoes  VARCHAR(30) NOT NULL
+);
+
+-- DADOS DE CARGOS
+ 
+INSERT INTO Cargos(funcoes)
+VALUES 
+	('Diretor'),
+	('Gerente'),
+	('Supervisor'),
+	('Assistente Geral'),
+	('Atendente'),
+    ('Suporte TI');
+
+SELECT * FROM Cargos;
+
+DESCRIBE Cargos;
 
 -- CRIAÇÃO DA TABELA 'FUNCIONÁRIOS'
  
@@ -62,14 +90,14 @@ CREATE TABLE Funcionarios(
     senha            VARCHAR(30),
     data_contratacao VARCHAR(20),
     id_cargo         INT,
-    FOREIGN KEY (id_cargo) REFERENCES Cargo(id_cargo)
+    CONSTRAINT fk_id_cargo FOREIGN KEY (id_cargo) REFERENCES Cargos(id_cargo)
 );
 
 -- SELECT PARA VISUALIZAR OS DADOS DOS FUNCIONARIOS
 
-SELECT f.cpf, f.nome, f.telefone, f.login, f.senha, f.data_contratacao, c.funcoes AS cargo
+SELECT f.cpf, f.nome, f.telefone, f.login, f.senha, f.data_contratacao, c.funcoes AS cargos
 FROM FUNCIONARIOS f
-JOIN cargo c ON f.id_cargo = c.id_cargo;
+JOIN cargos c ON f.id_cargo = c.id_cargo;
 
 -- DADOS DE FUNCIONARIO
 
@@ -83,22 +111,22 @@ VALUES
 
 -- CRIAÇÃO DA TABELA 'SETOR'
  
-CREATE TABLE Setor (
-    id_setor INT PRIMARY KEY,
+CREATE TABLE Setores (
+    id_setor INT(10) AUTO_INCREMENT PRIMARY KEY,
     setor    VARCHAR(40)
 );
 
 -- DADOS DOS FONECEDORES
 
-INSERT INTO Setor (id_setor, setor)
+INSERT INTO Setores (setor)
 VALUES 
-	(01, 'Fornecedor Filmes'),
-	(02, 'Fornecedor Jogos'),
-	(03, 'Fornecedor Fichas'),
-	(04, 'Fornecedor Arcade'),
-	(05, 'Fornecedor Alimentação');
+	('Fornecedor Filmes'),
+	('Fornecedor Jogos'),
+	('Fornecedor Arcade');
+    
+SELECT * FROM Setores;
 
--- CRIAÇÃO DA TABELA 'FORNECEDOR' 
+-- CRIAÇÃO DA TABELA 'FORNECEDOR'
  
 CREATE TABLE Fornecedor(
     cnpj     VARCHAR(30) PRIMARY KEY,
