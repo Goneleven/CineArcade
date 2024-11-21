54912358dec3a1d3760060e12e7cce7d5042b559
Cassiano Jesus da Silva <116390426+Ashketchup13@users.noreply.github.com>
2024-11-20 22:50:26 -0300

    Add files via upload

--- CineArcade.sql
+++ CineArcade.sql
@@ -79,33 +79,57 @@ CREATE TABLE Clientes(
 
 INSERT INTO Clientes(cpf, nome, telefone, email, data_nascimento, id_endereco_cliente) 
 VALUES 
-	('104.092.918-46', 'José Santana Pereira', '11 2350-7973', 'josesantana004@outlook.com', '1974-03-17', @id_endereco1),
-    ('099.784.428-04', 'Elza Sophia Lúcia Corte Real', '11 2512-1938', 'Elzasophiaa123@gmail.com', '1956-02-20', @id_endereco2),
-    ('255.527.438-30', 'Manoel Ryan Yago Rocha', '11 3543-8447', 'Manoelryanrocha@outlook.com', '1985-07-13', @id_endereco3),
-    ('826.387.678-83', 'Emanuel Rafael da Rocha', '11 2670-0066', 'Emanuelrocha444@gmail.com', '1988-08-19', @id_endereco4),
-    ('715.395.898-38', 'Lara Caroline Souza', '11 2791-2532', 'Laracarolsouzaa@gmail.com', '1999-01-11', @id_endereco5);
+	('104.092.918-46', 'José Santana Pereira', '11 92350-7973', 'josesantana004@outlook.com', '1974-03-17', @id_endereco1),
+    ('099.784.428-04', 'Elza Sophia Lúcia Corte Real', '11 92512-1938', 'Elzasophiaa123@gmail.com', '1956-02-20', @id_endereco2),
+    ('255.527.438-30', 'Manoel Ryan Yago Rocha', '11 93543-8447', 'Manoelryanrocha@outlook.com', '1985-07-13', @id_endereco3),
+    ('826.387.678-83', 'Emanuel Rafael da Rocha', '11 92670-0066', 'Emanuelrocha444@gmail.com', '1988-08-19', @id_endereco4),
+    ('715.395.898-38', 'Lara Caroline Souza', '11 92791-2532', 'Laracarolsouzaa@gmail.com', '1999-01-11', @id_endereco5);
+    
+    
 
 SELECT * FROM Clientes;
 
 DESCRIBE Clientes;
 
+-- CRIAÇÃO DA VIEW DO ENDEREÇO E CLIENTES
+
+CREATE VIEW v_clientes_endereco AS
+SELECT 
+         c.cpf,
+         c.nome AS cliente,
+         c. telefone,
+         c.email,
+         e.logradouro,
+         e.cep,
+         e.numero,
+         e.cidade,
+         e.uf
+FROM     Clientes c
+JOIN     Enderecos e ON c.id_endereco_cliente = e.id_enderecos
+ORDER BY c.nome;
+
+-- SELECT PARA VISUALIZAR A VIEW CLIENTE E ENDEREÇO
+
+SELECT * FROM v_clientes_endereco;
+
+
 -- CRIAÇÃO DA TABELA 'CARGOS' 
  
 CREATE TABLE Cargos(
-    id_cargo INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id_cargo INT PRIMARY KEY NOT NULL,
     funcoes  VARCHAR(30) NOT NULL
 );
 
 -- DADOS DE CARGOS
  
-INSERT INTO Cargos(funcoes)
+INSERT INTO Cargos(id_cargo, funcoes)
 VALUES 
-	('Diretor'),
-	('Gerente'),
-	('Supervisor'),
-	('Assistente Geral'),
-	('Atendente'),
-    ('Suporte TI');
+	(1, 'Diretor'),
+	(2, 'Gerente'),
+	(3, 'Supervisor'),
+	(4, 'Suporte TI'),
+	(5, 'Assistente Geral'),
+	(6, 'Atendente');
 
 SELECT * FROM Cargos;
 
@@ -134,12 +158,30 @@ JOIN cargos c ON f.id_cargo = c.id_cargo;
 
 INSERT INTO Funcionarios (cpf, nome, telefone, login, senha, data_contratacao, id_cargo)
 VALUES 
-	('220.959.228-32', 'Raimundo Filipe Lucas Almada', '11 35510-6125', 'Rai4763!', '47861@', '2023-07-10', 03),
-	('765.709.178-97', 'Ryan Igor Thiago Silva', '11 39818-4192', 'Ryan00797@', '4709Ryan@', '2023-06-15', 02),
-	('589.075.478-58', 'Aline Milena das Neves', '11 39631-3393', 'Aline1106@', '07976Aline!', '2023-01-20', 01),
-	('148.091.898-99', 'Leonardo Ryan Figueiredo', '11 26482-6969', 'Leo1212@', '07797@', '2023-04-05', 04),
-	('259.620.088-04', 'Davi Lucca Aparício', '11 38663-3326', 'Davi0079lucca@', '70970@', '2023-03-10', 05);
+	('220.959.228-32', 'Raimundo Filipe Lucas Almada', '11 95510-6125', 'Rai4763!', '47861@', '2023-07-10', 03),
+	('765.709.178-97', 'Ryan Igor Thiago Silva', '11 99818-4192', 'Ryan00797@', '4709Ryan@', '2023-06-15', 02),
+	('589.075.478-58', 'Aline Milena das Neves', '11 99631-3393', 'Aline1106@', '07976Aline!', '2023-01-20', 01),
+	('148.091.898-99', 'Leonardo Ryan Figueiredo', '11 96482-6969', 'Leo1212@', '07797@', '2023-04-05', 05),
+	('259.620.088-04', 'Davi Lucca Aparício', '11 98663-3326', 'Davi0079lucca@', '70970@', '2023-03-10', 06),
+    ('648.559.478-73', 'Yan Baumgarten Costa', '11 94745-4113', 'YanTI001@', 'Yan716479@!', '2023-01-05', 04);
+    
+-- VIEW PARA OS CARGOS E FUNCIONÁRIOS
+    
+CREATE VIEW v_cargo_funcionario AS
+SELECT 
+		 f.cpf,
+         nome AS funcionário, 
+         telefone,
+         data_contratacao AS 'Inicio de Contrato',
+         c.funcoes AS cargo
+FROM     Funcionarios f
+JOIN     Cargos c ON f.id_cargo = c.id_cargo
+ORDER BY f.data_contratacao DESC;
 
+-- SELECT PARA CARGOS E FUNCIONÁRIOS
+
+SELECT * FROM v_cargo_funcionario;
+        
 -- CRIAÇÃO DA TABELA 'SETOR'
  
 CREATE TABLE Setores (
@@ -186,6 +228,22 @@ SELECT f.cnpj, f.nome, f.telefone, f.email, f.id_endereco_fornecedor AS Endereç
 FROM Fornecedor f
 JOIN Setores s ON f.responsavel_setor = s.id_setor;
 
+-- VIEW SETOR E FORNECEDOR
+
+CREATE VIEW v_fornecedor_setor AS
+SELECT    f.cnpj,
+          f.nome AS fornecedor,
+          f.telefone, 
+          f.email,
+          s.setor
+FROM      Fornecedor f
+JOIN      Setores s ON f.responsavel_setor = s.id_setor
+ORDER BY  s.setor, f.nome;     
+
+-- SELECT PARA VISUALIZAR A VIEW SETOR E FORNECEDOR
+
+SELECT * FROM v_fornecedor_setor;
+
 -- CRIAÇÃO DA TABELA 'ARCADE' 
  
 CREATE TABLE Maquinas(
@@ -197,11 +255,11 @@ CREATE TABLE Maquinas(
 
 INSERT INTO Maquinas(id_arcade, nome)
 VALUES 
-	(01,'Pac-Man'),
-	(02,'Donkey Kong'),
-	(03,'Space Invaders'),
-	(04,'Street Fighter II'),
-	(05,'Galaga');
+	(1,'Pac-Man'),
+	(2,'Donkey Kong'),
+	(3,'Space Invaders'),
+	(4,'Street Fighter II'),
+	(5,'Galaga');
 
 -- CRIAÇÃO DA TABELA 'MIDIAS'
 
@@ -245,11 +303,11 @@ CREATE TABLE Fichas(
 
 INSERT INTO Fichas(id_fichas, quantidade, valor_total)
 VALUES 
-	(01, 500, 2500.00),
-	(02, 100, 400.00),
-	(03, 250, 1625.00),
-	(04, 400, 4000.00),
-	(05, 100, 350.00);
+	(1, 500, 5 * 500),
+	(2, 100, 5 * 100),
+	(3, 250, 5 * 250),
+	(4, 400, 5 * 400),
+	(5, 100, 5 * 100);
 
 SELECT * FROM Fichas;
 
@@ -266,27 +324,70 @@ CREATE TABLE Pedido(
     CONSTRAINT fk_id_midia FOREIGN KEY(id_midia) REFERENCES Midias(id_midia)
 );
 
--- Exemplo para pedidos com multa (caso atrasem)
+-- INSERT PEDIDO
+
 INSERT INTO Pedido(quantidade_pedida, valor_total, data_retirada, data_prevista_devolucao, data_efetiva_devolucao, id_midia)
 VALUES 
-    (2, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 1), '2024-11-16', DATE_ADD('2024-11-16', INTERVAL 10 DAY), '2024-11-20', 1),
-    (3, 3 * (SELECT valor_unitario FROM Midias WHERE id_midia = 2), '2024-11-15', DATE_ADD('2024-11-15', INTERVAL 14 DAY), '2024-11-17', 2), 
-    (1, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 4), '2024-11-14', DATE_ADD('2024-11-14', INTERVAL 12 DAY), '2024-11-20', 4),
-    (4, 4 * (SELECT valor_unitario FROM Midias WHERE id_midia = 3), '2024-11-13', DATE_ADD('2024-11-13', INTERVAL 20 DAY), NULL, 3),
-    (5, 5 * (SELECT valor_unitario FROM Midias WHERE id_midia = 10), '2024-10-12', DATE_ADD('2024-10-12', INTERVAL 7 DAY), '2024-10-22', 10);
+    (2, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 1), '2024-11-01', DATE_ADD('2024-11-01', INTERVAL 10 DAY), '2024-11-10', 1),
+    (3, 3 * (SELECT valor_unitario FROM Midias WHERE id_midia = 6), '2024-10-25', DATE_ADD('2024-10-25', INTERVAL 12 DAY), '2024-11-06', 6),
+    (1, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 3), '2024-11-02', DATE_ADD('2024-11-02', INTERVAL 15 DAY), '2024-11-17', 3),
+    (2, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 9), '2024-10-31', DATE_ADD('2024-10-31', INTERVAL 7 DAY), '2024-11-07', 9),
+    (1, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 4), '2024-11-10', DATE_ADD('2024-11-10', INTERVAL 20 DAY), '2024-11-24', 4),
+    (2, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 2), '2024-10-15', DATE_ADD('2024-10-15', INTERVAL 15 DAY), '2024-11-05', 2),
+    (1, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 7), '2024-10-10', DATE_ADD('2024-10-10', INTERVAL 20 DAY), NULL, 7),
+    (2, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 8), '2024-09-25', DATE_ADD('2024-09-25', INTERVAL 14 DAY), '2024-10-10', 8);
+
+-- UPDATE PEDIDO ATUALIZAÇÃO
 
 UPDATE Pedido
 SET 
     valor_total = valor_total + 
     (CASE 
         WHEN data_efetiva_devolucao > data_prevista_devolucao THEN 
-            DATEDIFF(data_efetiva_devolucao, data_prevista_devolucao) * 0.01 * valor_total
+            DATEDIFF(data_efetiva_devolucao, data_prevista_devolucao) * 0.08 * valor_total
         ELSE 
             0 
     END)
 WHERE data_efetiva_devolucao IS NOT NULL AND data_efetiva_devolucao > data_prevista_devolucao;
 
+-- SELECT PARA VISUALIZAR MULTA
+
+SELECT
+    p.id_pedido, 
+    p.quantidade_pedida, 
+    p.valor_total, 
+    p.data_retirada, 
+    p.data_prevista_devolucao, 
+    DATEDIFF(CURDATE(), p.data_prevista_devolucao) AS dias_de_atraso,
+    CONCAT('R$', FORMAT(DATEDIFF(CURDATE(), p.data_prevista_devolucao) * 0.08 * p.valor_total, 2)) AS multa
+FROM 
+    Pedido p
+WHERE 
+    p.data_efetiva_devolucao IS NULL 
+    AND p.data_prevista_devolucao < CURDATE()
+ORDER BY 
+    p.data_prevista_devolucao;
+
+-- SELECT PEDIDO
+
 SELECT* FROM Pedido;
+
+-- VIEW PARA PEDIDOS E MIDIAS 
+
+CREATE VIEW v_pedidos_midias AS
+SELECT 
+    p.id_pedido,
+    p.quantidade_pedida,
+    p.valor_total,
+    p.data_retirada,
+    p.data_prevista_devolucao,
+    m.nome AS midia
+FROM Pedido p
+JOIN Midias m ON p.id_midia = m.id_midia
+ORDER BY p.data_retirada;
+
+-- Visualizar a View
+SELECT * FROM v_pedidos_midias;
 
 DESCRIBE Pedido;
 
@@ -295,19 +396,23 @@ DESCRIBE Pedido;
 CREATE TABLE Produto(
     id_produto INT PRIMARY KEY,
     id_fichas INT,
+    quantidade_fichas INT,
     id_pedido INT,
     valor_total DECIMAL(6, 2),
     CONSTRAINT fk_id_fichas FOREIGN KEY(id_fichas) REFERENCES Fichas(id_fichas),
     CONSTRAINT fk_id_pedido FOREIGN KEY(id_pedido) REFERENCES Pedido(id_pedido)
 );
 
-INSERT INTO Produto(id_produto, valor_total, id_fichas, id_pedido)
+INSERT INTO Produto(id_produto, valor_total, id_fichas, quantidade_fichas, id_pedido)
 VALUES 
-    (1, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 1), 1, 1),
-    (2, 3 * (SELECT valor_unitario FROM Midias WHERE id_midia = 2), 2, 4),
-    (3, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 4), 3, 5),
-    (4, 4 * (SELECT valor_unitario FROM Midias WHERE id_midia = 3), 4, 2),
-    (5, 5 * (SELECT valor_unitario FROM Midias WHERE id_midia = 10), 5, 3);
+    (1, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 1), NULL, NULL, 1), 
+    (2, 3 * (SELECT valor_unitario FROM Midias WHERE id_midia = 6), 1, 8, 2), 
+    (3, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 3), 2, 10, 3), 
+    (4, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 9), 5, 3, 4), 
+    (5, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 4), 3, 1, 5), 
+    (6, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 2), NULL, NULL, 6), 
+    (7, 1 * (SELECT valor_unitario FROM Midias WHERE id_midia = 7), 4, 1, 7), 
+    (8, 2 * (SELECT valor_unitario FROM Midias WHERE id_midia = 8), 3, 7, 8); 
 
 -- CRIAÇÃO DA TABELA 'MÉTODO DE PAGAMENTO'
 
@@ -333,15 +438,20 @@ DESCRIBE Metodo_De_Pagamento;
 -- CRIAÇÃO DA TABELA 'COMANDA'
  
 CREATE TABLE Comanda(
-    id_comanda      INT PRIMARY KEY AUTO_INCREMENT,
-    data            DATE,
-    horario         TIME,
-    id_pagamento	INT NOT NULL,
-    cpf_cliente		 VARCHAR(14) NOT NULL,
-    cpf_funcionarios	VARCHAR(14) NOT NULL,
-    CONSTRAINT fk_ide_pagamento FOREIGN KEY (id_pagamento) REFERENCES Metodo_De_Pagamento(id_pagamento),
+    id_comanda          INT PRIMARY KEY AUTO_INCREMENT,
+    data                DATE,
+    horario             TIME,
+    fichas              INT,  
+    Total               DECIMAL(6, 2),
+    id_pagamento        INT NOT NULL,
+    cpf_cliente         VARCHAR(14) NOT NULL,
+    cpf_funcionarios    VARCHAR(14) NOT NULL,
+    valor_total_produto DECIMAL(6, 2),
+    CONSTRAINT fk_id_pagamento FOREIGN KEY (id_pagamento) REFERENCES Metodo_De_Pagamento(id_pagamento),
     CONSTRAINT fk_cpf_cliente FOREIGN KEY (cpf_cliente) REFERENCES Clientes(cpf),
-    CONSTRAINT fk_cpf_funcionarios FOREIGN KEY (cpf_funcionarios) REFERENCES Funcionarios(cpf)
+    CONSTRAINT fk_cpf_funcionarios FOREIGN KEY (cpf_funcionarios) REFERENCES Funcionarios(cpf),
+    CONSTRAINT fk_fichas FOREIGN KEY (fichas) REFERENCES Produto(quantidade_fichas),
+    CONSTRAINT fk_total FOREIGN KEY (Total) REFERENCES Produto(valor_total) 
 );
 
 -- Inserir dados de exemplo na tabela Comanda
@@ -356,5 +466,25 @@ VALUES
 SELECT c.id_comanda, c.data, c.horario, c.id_pagamento, c.cpf_cliente, f.nome AS nome_funcionario
 FROM Comanda c
 JOIN Funcionarios f ON c.cpf_funcionarios = f.cpf;
+
+-- VIEW PARA COMANDOS E PAGAMENTO
+
+CREATE VIEW v_comandas_pagamento AS
+SELECT 
+    co.id_comanda,
+    co.data,
+    co.horario,
+    mp.forma_de_pagamento,
+    c.nome AS cliente,
+    f.nome AS funcionario
+FROM Comanda co
+JOIN Metodo_De_Pagamento mp ON co.id_pagamento = mp.id_pagamento
+JOIN Clientes c ON co.cpf_cliente = c.cpf
+JOIN Funcionarios f ON co.cpf_funcionarios = f.cpf
+ORDER BY co.data DESC;
+
+-- SELECT PARA VISUALIZAR COMANDAS E PAGAMENTO
+
+SELECT * FROM v_comandas_pagamento;
 
 DESCRIBE Comanda;
